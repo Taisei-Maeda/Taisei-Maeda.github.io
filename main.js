e5a013d156730890675dd133f24237096b3a67d4
Taisei-Maeda <58406963+Taisei-Maeda@users.noreply.github.com>
2019-12-10 19:58:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -37,20 +37,30 @@ $('a[href^=#]').click(function(){
   $('body').animate({scrollTop: targetY}, 500, 'swing');
 });
 
-/**
- * animatedクラスを持つ要素が画面内に入ったら
- * Animate.cssのfadeInUpエフェクトを適用
- */
-$('.animated').waypoint({
-  handler(direction) {
-    if (direction === 'down') {
-      $(this.element).addClass('fadeInUp');
-      this.destroy();
-    }
-  },
-  /**
-   * 要素の上端が画面のどの位置に来たときにhandlerメソッドを呼び出すか指定
-   * 0%なら画面の一番上、100%なら画面の一番下に来たときに呼び出される
-   */
-  offset: '100%',
+
+// $('.animated').waypoint({
+//   handler(direction) {
+//     if (direction === 'down') {
+//       $(this.element).addClass('fadeInUp');
+//       this.destroy();
+
+$(document).ready(function() {
+  
+  $('.wpu').waypoint(function() {
+    $('.wpu').addClass('animated fadeInUp');
+  }, {
+    offset: '80%'
+  });
+  $('.wpr').waypoint(function() {
+    $('wpr').addClass('animated slideInRight');
+  }, {
+    offset: '80%'
+  });
+  $('.wpl').waypoint(function() {
+    $('wpl').addClass('animated slideInLeft');
+  }, {
+    offset: '80%'
+  });
+
 });
+
