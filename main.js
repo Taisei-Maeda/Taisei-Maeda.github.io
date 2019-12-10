93b17ec481202f87b6866437e1267dd8a9328767
Taisei-Maeda <58406963+Taisei-Maeda@users.noreply.github.com>
2019-12-10 20:11:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ $('a[href^=#]').click(function(){
 
 
 // animatedクラスの付いた要素にwaypointを登録
-$('.animated').waypoint({
+$('.wp').waypoint({
   handler(direction) {
     // 要素が画面中央に来るとここが実行される
     if (direction === 'down') {
@@ -48,7 +48,7 @@ $('.animated').waypoint({
        * イベント発生元の要素にfadeInUpクラスを付けることで
        * アニメーションを開始
        */
-      $(this.element).addClass('fadeInUp');
+      $(this.element).addClass('animated fadeInUp');
 
       /**
        * waypointを削除することで、この要素に対しては
@@ -59,5 +59,5 @@ $('.animated').waypoint({
   },
 
   // 要素が画面中央に来たらhandlerを実行
-  offset: '50%',
+  offset: '80%',
 });
